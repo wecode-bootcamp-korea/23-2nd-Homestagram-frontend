98a544e9e470babb3e0d23cad181174efad72e7d
ddodam <80012897+ddodam@users.noreply.github.com>
2021-08-19 15:45:07 +0900

    [ADD] detail_layout 완료
    [FIX] 레이아웃 리뷰 반영/기능구현 1차 완료
    
    Add: detail 페이지 기능 완료
    
    이미지 파일 삭제
    
    img size 조정
    
    주석삭제
    
    Fix 리뷰적용/로직 수정
    
    Fix 리뷰 반영
    
    Fix 리뷰반영수정

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SideImg from './SideImg';
+import DetailInfo from './DetailInfo';
+import styled from 'styled-components';
 
 const DetailPage = () => {
-  return <div>상세정보</div>;
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('./data/data.json')
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => console.err(err));
+  }, []);
+
+  return (
+    <Wrap>
+      <SideImg data={data} />
+      <DetailInfo data={data} />
+    </Wrap>
+  );
 };
+const Wrap = styled.main`
+  display: flex;
+  width: 1136px;
+  margin: 170px auto;
+`;
+
 export default DetailPage;
