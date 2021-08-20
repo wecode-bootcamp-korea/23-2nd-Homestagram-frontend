1ba0e37b2364b158049a9ef6844faa47981aaf04
남재현 <nam2350@gmail.com>
2021-08-20 15:02:40 +0900

    Add: 결제페페이지 셋팅 ~ 결제페이지 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './component/Nav/Nav';
-// import SignPage from './pages/SignPage/SignPage';
 import Feeds from './pages/Feeds/Feeds';
 import MyPage from './pages/MyPage/MyPage';
 import DetailPage from './pages/DetailPage/DetailPage';
 import Footer from './component/Footer/Footer';
+import OrderPage from './pages/OrderPage/OrderPage';
 
 const Routes = () => {
   return (
@@ -13,9 +13,9 @@ const Routes = () => {
       <Nav />
       <Switch>
         <Route exact path="/" component={Feeds} />
-        {/* <Route exact path="/signpage" component={SignPage} /> */}
         <Route exact path="/mypage" component={MyPage} />
         <Route exact path="/detailpage" component={DetailPage} />
+        <Route exact path="/orderpage" component={OrderPage} />
       </Switch>
       <Footer />
     </Router>
